0aa3e2925a00a9005b0ff4993717e4b6f0dacd10
boaz42 <kemboiboaz42@gmail.com>
2023-04-30 17:54:02 +0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia
+        expedita voluptatum quam autem doloribus blanditiis. Fugit obcaecati
+        praesentium accusantium nihil, minus quod accusamus dolores officiis
+        aspernatur eum beatae, ratione veniam iusto quis sapiente cupiditate et
+        facilis ipsum vitae consectetur iure!
+      </p>
+    </section>
+  );
+};
+
+export default About;
